85cacce27cb03967312c2b3eeb258f1bc14544f4
MarinaSakai <pavane.ravel.5m2t0.conamore@gmail.com>
2018-05-31 02:28:41 +0900

    一部の文字+点数の高い単語

--- ドキュメント/GoogleSTEP/0525/Anagram.java
+++ ドキュメント/GoogleSTEP/0525/Anagram.java
@@ -12,7 +12,8 @@ public class Anagram {
     BufferedReader br = new BufferedReader(isr);
     String letters;
     String[] lettersArray = null;
-    String lettersSort;
+    String[] words = new String[10000];
+    int n = 0;
   try {
       letters = br.readLine();
       lettersArray = letters.split("");
@@ -20,69 +21,72 @@ public class Anagram {
       System.out.println(e);
     }
     Arrays.sort(lettersArray); // sort
-    lettersSort = String.join("", lettersArray);
+
 
     // Dictionaryの内容を読み込んでsortした状態でdicDataListSortに格納
     String file_name = "Dictionary.txt";
     Dictionary dicScan = new Dictionary(new File(file_name));
-//    ArrayList<String[]> dicData = dicScan.read();
-    String[] dicData = dicScan.read(); // Dictionaryの内容(String)
+    String[] dicData = dicScan.read(); // Dictionaryの内容
     int count = 0;
     for(int i = 0; i < dicData.length; i ++) {
       if(dicData[i] == null) break;
       count ++;
     }
-    System.out.println("count: " + count); //72412
+
     String[] dicDataArray = new String[16]; // Dictionaryの単語自体をsortするために一度配列に変換
-    String[] dicDataList = new String[count]; // Dictionaryの単語自体のみがsortされたリスト
-    String[] dicDataListSort = new String[count]; // Dictionaryの単語同士もsortされたリスト
     for(int i = 0; i < count; i ++) {
-      if(dicData[i] != null) dicDataArray = dicData[i].split("");
-      Arrays.sort(dicDataArray);
-      dicDataList[i] = String.join("", dicDataArray);
-      dicDataListSort[i] = dicDataList[i];
+//      System.out.println(dicData[i]);
+      if(dicData[i] != null) dicDataArray = dicData[i].split(""); // 一度配列に
+      Arrays.sort(dicDataArray); // SortArray
+
+      // Search
+      int c = 0;
+      int p = 0;
+      for(int j = 0; j < dicDataArray.length; j ++) {
+        if(dicDataArray.length > lettersArray.length) break;
+        for(int k = p; k < lettersArray.length; k ++) {
+          if(lettersArray[k].equals(dicDataArray[j])) {
+//            System.out.println("l: " + lettersArray[k] + " d: " + dicDataArray[j]);
+            c ++;
+            p = k+1;
+            break;
+          }
+        }
+      }
+      if(c == dicDataArray.length) {
+        words[n] = dicData[i];
+        n ++;
+        System.out.printf(dicData[i] + " ");
+      }
     }
-    Arrays.sort(dicDataListSort);
+    System.out.printf("\n");
+
+    // BEST SCORE
+    System.out.println(n);
+    if(n > 0) {
+      int num = -1;
+      int max = 0;
+      for(int i = 0; i < n; i ++) {
+        int score = 0;
+        String[] str = words[i].split("");
+        for(int j = 0; j < str.length; j ++) {
+          if(str[j].equals("a") || str[j].equals("b") || str[j].equals("d") || str[j].equals("e") || str[j].equals("g") || str[j].equals("i") || str[j].equals("n") || str[j].equals("o") || str[j].equals("r") || str[j].equals("s") || str[j].equals("t") || str[j].equals("u")) {
+            score ++;
+          } else if(str[j].equals("c") || str[j].equals("f") || str[j].equals("h") || str[j].equals("l") || str[j].equals("m") || str[j].equals("p") || str[j].equals("v") || str[j].equals("w") || str[j].equals("y")) {
+            score += 2;
+          } else if(str[j].equals("j") || str[j].equals("k") || str[j].equals("q") || str[j].equals("x") || str[j].equals("z")) {
+            score += 3;
+          }
+        }
+        if(score > max) {
+          max = score;
+          num = i;
+        }
+      }
+      System.out.println("THE BEST: " + words[num] + max);
+    }
 
     dicScan.close();
 
-    System.out.println(lettersSort);
-    int num;
-    int none = 0;
-    System.out.println(dicDataList.length);
-    for(num = 0; num < dicDataList.length; num ++) {
-      if(lettersSort.compareToIgnoreCase(dicDataList[num]) == 0) {
-        break;
-      }
-      if(num == dicDataList.length-1) {
-        System.out.println("none");
-        none = 1;
-        break;
-      }
-    }
-    if(none == 0) {
-      System.out.println(dicDataList[num]+" num: " + num);
-      System.out.println(dicData[num]);
-    }
-
-/*    // 比較(二分探索)
-    int L = 0;
-    int R = dicDataListSort.length;
-    int M = -1;
-    String str;
-    while(L < R) {
-      M = (L+R)/2;
-      str = dicDataListSort[M];
-      System.out.println(str);
-      if(lettersSort.compareToIgnoreCase(str) < 0) { // targetの方が小さい
-        R = M;
-      } else if(lettersSort.compareToIgnoreCase(str) > 0) { // targetの方が大きい
-        L = M + 1;
-      } else { // targetと等しい
-        break;
-      }
-    }
-    System.out.println("The word is " + dicDataListSort[M]);
-    */
   }
 }
